1feaf98a373a9c27890a5f7a8598c1bb0f308cc3
PitouBot <dima72793@gmail.com>
2026-07-26 16:55:16 +0400

    Подправил текст в help.py

--- handlers/help.py
+++ handlers/help.py
@@ -9,12 +9,17 @@ logger = logging.getLogger(__name__)
 
 @router.message(Command("help"))
 async def cmd_help(message: Message):
-    await message.answer("Доступные команды:\n/start - приветствие\n/help - помощь")
+    await message.answer(
+        "Доступные команды:\n/start - приветствие\n/help - помощь\n/all_notes - показать все заметки\n"
+        "Для дополнительных функций можно использовать информативную клавиатуру бота",
+        reply_markup=main_menu
+    )
 
 @router.callback_query(F.data == 'help')
 async def cmd_help(callback: CallbackQuery):
     await callback.answer()
     await callback.message.edit_text(
-        "Доступные команды:\n/start - приветствие\n/help - помощь",
+        "Доступные команды:\n/start - приветствие\n/help - помощь\n/all_notes - показать все заметки\n"
+        "Для дополнительных функций можно использовать информативную клавиатуру бота",
         reply_markup=main_menu
     )
